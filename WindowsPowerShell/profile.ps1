249cbab6791283b33ebc977342cca635d2b3fad3
carlosap <perezca6576@yahoo.com>
2019-10-03 18:09:16 -0400

    init

---
+++ WindowsPowerShell/profile.ps1
@@ -0,0 +1,10 @@
+# Welcome message
+"You are now entering PowerShell : " + $env:Username
+
+#repos folder
+#$localpath = Join-Path $env:USERPROFILE 'some\path'
+function repos { set-location "C:\Users\$env:Username\source\repos" }
+function home { set-location $env:HOMEPATH }
+
+#show env vars
+#Set-Alias -Name env -Value "gci env:* | sort-object name"
