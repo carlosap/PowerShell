cd762412303591ef346f3be681d8626a1b81f392
carlosap <perezca@yahoo.com>
2019-10-03 18:51:24 -0400

    Add nodejs installer

--- WindowsPowerShell/profile.ps1
+++ WindowsPowerShell/profile.ps1
@@ -5,6 +5,7 @@
 #$localpath = Join-Path $env:USERPROFILE 'some\path'
 function repos { set-location "C:\Users\$env:Username\source\repos" }
 function home { set-location $env:HOMEPATH }
+function doc { set-location "C:\Users\$env:UserName\Documents" }
 
 #show env vars
 #Set-Alias -Name env -Value "gci env:* | sort-object name"
