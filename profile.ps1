8e972d9b0e20c4281dbce70f7ddf38cfd01f5368
carlosap <perezca@yahoo.com>
2020-01-10 08:53:08 -0500

    Add buffer size

--- profile.ps1
+++ profile.ps1
@@ -158,3 +158,21 @@ function gityahoo(){
     git config user.name
     git config user.email
 }
+
+#--------------------------------File Helpers Functions-----------------------------------
+function filesize($file) {
+   if($file) {
+    $size = Format-FileSize((Get-Item $file).length)
+    Write-Host("$file        size: $size")
+   }
+ }
+ 
+ Function Format-FileSize() {
+    Param ([int]$size)
+    If ($size -gt 1TB) {[string]::Format("{0:0.00} TB", $size / 1TB)}
+    ElseIf ($size -gt 1GB) {[string]::Format("{0:0.00} GB", $size / 1GB)}
+    ElseIf ($size -gt 1MB) {[string]::Format("{0:0.00} MB", $size / 1MB)}
+    ElseIf ($size -gt 1KB) {[string]::Format("{0:0.00} kB", $size / 1KB)}
+    ElseIf ($size -gt 0) {[string]::Format("{0:0.00} B", $size)}
+    Else {""}
+}
